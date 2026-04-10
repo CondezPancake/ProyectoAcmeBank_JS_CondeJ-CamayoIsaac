9e0ed9bbef431a7e22afc0146c2b5a48232646b8
Manuel Isaac Camaño Diaz <247783877+manuelisaaccamanidiaz-lgtm@users.noreply.github.com>
2026-04-10 06:39:21 -0500

    feat: :sparkles: login y storage js hechos y htmls
    hice el js de storage y log in,junto con los html del manejo de cuentas
    
    falta afinar algunos detalles y valiidaciones

--- JS/auth/login.js
+++ JS/auth/login.js
@@ -1,1 +1,20 @@
-.
+import * as funciones from "../core/storage.js";
+
+const tipoDocInput = document.querySelector(".tipoDoc").value;
+const numeroDocInput = document.querySelector(".numeroDoc");
+const passwordInput = document.querySelector(".password");
+const btnSunmitInput = document.querySelector(".btnSubmit");
+
+const usuario = funciones.obtenerUsuario(numeroDocInput);
+
+btnSunmitInput.addEventListener("click", ()=>{
+    if (!usuario) {
+    alert("Usuario no existe");
+    } else if (usuario.password === passwordInput) {
+    window.location.href = "principal.html";
+    } else {
+        alert("Contraseña incorrecta");
+    }
+
+    localStorage.setItem("usuarioActivo", JSON.stringify(usuario));
+}); 
--- JS/core/storage.js
+++ JS/core/storage.js
@@ -1,1 +1,38 @@
-.
+export function obtenerUsuario(documentoU) {
+    const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    if (!usuarios) {
+        return null
+    }
+
+    const usuario = usuarios.find(u => u.numeroId === documentoU);
+    return usuario;
+}
+
+export function guardarUsuario(usuario) {
+    let usuarios = JSON.parse(localStorage.getItem("usuarios"));
+    if (!usuarios) {
+        usuarios = [];
+    }
+    usuarios.push(usuario);
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+    return true;
+}
+
+export function obtenerTransacciones(numeroReferencia) {
+    const transferencias = JSON.parse(localStorage.getItem("transferencias"));
+    if (!transferencias) {
+        return []
+    }
+
+    const tranferencia = transferencias.slice(-10);
+    return tranferencia;
+}
+export function guardarTransacciones(transaccion) {
+    let transferencias = JSON.parse(localStorage.getItem("transferencias"));
+    if (!transferencias) {
+        transferencias = [];
+    }
+    transferencias.push(transaccion);
+    localStorage.setItem("transferencias", JSON.stringify(transferencias));
+    return true;
+}
